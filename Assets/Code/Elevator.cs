22c26df7df152926b711483becd48c7aa7814ec1
Unvios22 <unvios22@gmail.com>
2018-10-25 17:59:45 +0200

    Initial commit

---
+++ Assets/Code/Elevator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+	[SerializeField] private float _time;
+	private Transform _elevatorTransform;
+	private Vector3 _elevatorStart = new Vector3(0f, 0f, 0f);
+	private Vector3 _elevatorStop = new Vector3(0f, 10f, 0f);
+	private Vector3 _elevatorGround;
+	private bool _goingUp;
+	private bool _playerOnGround;
+	private Transform _playerTransform;
+
+	void Start() {
+		_elevatorTransform = this.transform;
+		_elevatorStart = _elevatorTransform.position;
+		_playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+		_elevatorGround = GameObject.FindGameObjectWithTag("ElevatorGround").transform.position;
+	}
+
+	private void OnTriggerStay(Collider collider) {
+		_elevatorTransform.position = Vector3.Lerp(_elevatorTransform.position, _elevatorStop, _time * Time.deltaTime);
+		_goingUp = true;
+		_playerOnGround = false;
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.CompareTag("Player")) {
+			_goingUp = false;
+		}
+	}
+
+	void Update() {
+		if (_playerTransform.position.y <= _elevatorGround.y + 1f) {
+			_playerOnGround = true;
+		}
+		if (!_goingUp) {
+			_elevatorTransform.position =
+				Vector3.Lerp(_elevatorTransform.position, _elevatorStart, _time * Time.deltaTime);
+			if (!_playerOnGround) {
+				_playerTransform.position = Vector3.Lerp(_playerTransform.position,
+					(new Vector3(_playerTransform.position.x, _elevatorStart.y, _playerTransform.position.z)),
+					_time * Time.deltaTime);
+			}
+		}
+	}
+}
---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [Range(1f, 100f)] [SerializeField] float movementSpeed = 1f;
+     float rotationSpeed = 1f;
+	private Animator _playerAnimator;
+
+	private void Start() {
+		_playerAnimator = gameObject.GetComponentInChildren<Animator>();
+	}
+
+	private void Update() {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f * rotationSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f * movementSpeed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+		if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Vertical") > 0) {
+
+			_playerAnimator.ResetTrigger("isStationary");
+
+			if (Input.GetKey(KeyCode.LeftShift)) {
+				_playerAnimator.SetTrigger("isRunning");
+				_playerAnimator.ResetTrigger("isMoving");
+			}
+			else if (Input.GetKey(KeyCode.C)) {
+				_playerAnimator.SetTrigger("isSneaking");
+				_playerAnimator.ResetTrigger("isMoving");
+			}
+			_playerAnimator.SetTrigger("isMoving");
+			
+		}
+		else {
+			_playerAnimator.SetTrigger("isStationary");
+			_playerAnimator.ResetTrigger("isMoving");
+			_playerAnimator.ResetTrigger("isSneaking");
+			_playerAnimator.ResetTrigger("isRunning");
+			
+		}
+    }
+	
+}
