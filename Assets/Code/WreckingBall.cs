8aabbe94ffe2a7826207a033240690973aa7b662
Unvios22 <unvios22@gmail.com>
2018-10-26 02:23:36 +0200

    Added wrecking ball mechanic

--- Assets/Code/WreckingBall.cs
+++ Assets/Code/WreckingBall.cs
@@ -2,15 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WreckingBall : MonoBehaviour {
+namespace Code{
+	public class WreckingBall : MonoBehaviour{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		[SerializeField] private float _ballForceBoost = 3f;
+		[SerializeField] private float _ballForce = 1f;
+
+		private Animator _characterAnimator;
+
+		private void OnCollisionEnter(Collision other){
+			if (other.gameObject.CompareTag("Player")){
+				ApplyRagdoll(other.gameObject);
+			}
+
+			Rigidbody collidedRigidbody = other.gameObject.GetComponent<Rigidbody>();
+			var vector = other.transform.position - transform.position;
+			var direction = vector.normalized;
+			collidedRigidbody.AddForce(direction * _ballForceBoost);
+
+			List<Rigidbody> childRigidbodies = ReturnChildRigidbodies(other.gameObject);
+			foreach (var rb in childRigidbodies){
+				rb.AddForce(direction * _ballForce * _ballForceBoost);
+			}
+		}
+
+		private void ApplyRagdoll(GameObject player){
+			_characterAnimator = player.GetComponentInChildren<Animator>();
+			_characterAnimator.enabled = false;
+			StartCoroutine(ResetCharacter());
+		}
+
+		private IEnumerator ResetCharacter(){
+			yield return new WaitForSeconds(3);
+			_characterAnimator.enabled = true;
+		}
+
+		private static List<Rigidbody> ReturnChildRigidbodies(GameObject parentGameObject){
+			var list = new List<Rigidbody>();
+			GetChildRecursive(parentGameObject, list);
+			return list;
+		}
+
+		private static void GetChildRecursive(GameObject obj, ICollection<Rigidbody> list){
+			if (obj == null){
+				return;
+			}
+
+			foreach (Transform child in obj.transform){
+				if (child == null){
+					continue;
+				}
+
+				var rigidbody = child.gameObject.GetComponent<Rigidbody>();
+				if (rigidbody != null){
+					list.Add(rigidbody);
+				}
+
+				GetChildRecursive(child.gameObject, list);
+			}
+		}
 	}
 }
