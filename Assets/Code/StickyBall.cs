7d19667f7fd8586a7c83d5d2b74a25e01aeb02a3
Unvios22 <unvios22@gmail.com>
2018-10-26 11:30:46 +0200

    Added StickyBall script

---
+++ Assets/Code/StickyBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyBall : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other) {
+		if (other.transform.CompareTag("StickyWall")) {
+			var rigidbody = other.gameObject.GetComponent<Rigidbody>();
+			if (rigidbody == null) {
+				return;
+			}
+			var joint = gameObject.AddComponent<FixedJoint>();
+			joint.connectedBody = rigidbody;
+		}
+	}
+}
